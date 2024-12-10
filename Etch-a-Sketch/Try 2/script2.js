a7fd3be5be6ec5ec6813124891973eea0337932d
Ashmit Bindal <Ashmit72061@gmail.com>
2024-12-10 15:26:29 +0530

    Added more natural drawing thing click to start drawing

--- Etch-a-Sketch/Try 2/script2.js
+++ Etch-a-Sketch/Try 2/script2.js
@@ -11,7 +11,7 @@ console.log(widthOfGrid);
 for(let i = 1; i <= noOfGrid*noOfGrid;++i){
     const grid = document.createElement("div");
     // grid.innerHTML = i;
-    grid.setAttribute("style", `width: ${widthOfGrid}px; border: 2px solid black; box-sizing: border-box;`);
+    grid.setAttribute("style", `width: ${widthOfGrid}px; border: 1px solid black; box-sizing: border-box;`);
     container.appendChild(grid);
 }
 
@@ -23,12 +23,25 @@ red.addEventListener("click", ()=>{
 document.querySelector(".blue").addEventListener("click", ()=>{
     color = "blue";
 });
+document.querySelector(".eraser").addEventListener("click", ()=>{
+    color = "white";
+});
+
+let isMouseDown = false;
 
 const allGrid = container.querySelectorAll("*");
 for(box of allGrid){
 
-    box.addEventListener("mouseover", (e)=>{
+    box.addEventListener("mousedown", (e)=> {
+        isMouseDown = true;
         e.target.style.background = `${color}`;
+    });
+    box.addEventListener("mouseup", ()=> isMouseDown = false);
+
+    box.addEventListener("mouseover", (e)=>{
+        if(isMouseDown === true){
+            e.target.style.background = `${color}`;
+        }
     });
 }
 
@@ -37,3 +50,26 @@ function reload() {
 }
 
 document.querySelector(".grid").addEventListener("click", reload);
+
+function clear(){
+    for(dibba of allGrid){
+        dibba.style.background = "white";
+        console.log(dibba);
+    }
+}
+
+document.querySelector(".clear").addEventListener("click", clear);
+
+
+function removeAllBoxes(){
+    for(dibba of allGrid){
+        dibba.remove();
+    }
+}
+
+/*
+To change no of grid 
+    1. remove all pre existing boxes
+    2. create new box and add event listener to them at the same time
+    3. append those boxes
+*/
