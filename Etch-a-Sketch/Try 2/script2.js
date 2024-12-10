14f32b98eb749d80acc8cf9981e9957822fec0ad
Ashmit Bindal <Ashmit72061@gmail.com>
2024-12-10 16:46:24 +0530

    Cleaned up a the code, made creation of grid a function and created grid everytime instead of reloading the site hopefully add some css in the future

--- Etch-a-Sketch/Try 2/script2.js
+++ Etch-a-Sketch/Try 2/script2.js
@@ -1,75 +1,72 @@
-let noOfGrid = prompt("No of grid?");
 const container = document.querySelector("#container");
+createGrid(container,16);
 
-// const widthOfContainer = container.getBoundingClientRect().width - 4;
-const widthOfContainer = container.clientWidth;
-const widthOfGrid = widthOfContainer/noOfGrid;
-console.log(`${widthOfContainer}`);
-
-console.log(widthOfGrid);
-
-for(let i = 1; i <= noOfGrid*noOfGrid;++i){
-    const grid = document.createElement("div");
-    // grid.innerHTML = i;
-    grid.setAttribute("style", `width: ${widthOfGrid}px; border: 1px solid black; box-sizing: border-box;`);
-    container.appendChild(grid);
-}
-
+//All Btns script
 let color = "green";
 const red = document.querySelector(".red");
-red.addEventListener("click", ()=>{
+red.addEventListener("click", () => {
     color = "red";
 });
-document.querySelector(".blue").addEventListener("click", ()=>{
+document.querySelector(".blue").addEventListener("click", () => {
     color = "blue";
 });
-document.querySelector(".eraser").addEventListener("click", ()=>{
+document.querySelector(".green").addEventListener("click", () => {
+    color = "green";
+});
+document.querySelector(".eraser").addEventListener("click", () => {
     color = "white";
 });
 
-let isMouseDown = false;
+document.querySelector(".grid").addEventListener("click", ()=>{
+    let x  = prompt("No of Grid?")    
+    createGrid(container,x);
+});
 
-const allGrid = container.querySelectorAll("*");
-for(box of allGrid){
-
-    box.addEventListener("mousedown", (e)=> {
-        isMouseDown = true;
-        e.target.style.background = `${color}`;
-    });
-    box.addEventListener("mouseup", ()=> isMouseDown = false);
-
-    box.addEventListener("mouseover", (e)=>{
-        if(isMouseDown === true){
-            e.target.style.background = `${color}`;
-        }
-    });
-}
-
-function reload() {
-    location.reload();
-}
-
-document.querySelector(".grid").addEventListener("click", reload);
-
-function clear(){
-    for(dibba of allGrid){
-        dibba.style.background = "white";
-        console.log(dibba);
+document.querySelector(".clear").addEventListener("click", ()=>{
+    const allGrid = container.querySelectorAll("*");
+    for(box of allGrid){
+        box.style.background = "white";
     }
-}
-
-document.querySelector(".clear").addEventListener("click", clear);
-
-
-function removeAllBoxes(){
-    for(dibba of allGrid){
-        dibba.remove();
-    }
-}
+});
 
 /*
-To change no of grid 
-    1. remove all pre existing boxes
-    2. create new box and add event listener to them at the same time
-    3. append those boxes
+To create grid 
+1. remove all pre existing boxes
+2. create new box and add event listener to them at the same time
+3. append those boxes
 */
+
+function removeAllBoxes(){
+    while(container.firstChild){
+        container.firstChild.remove();
+    }
+}
+
+function createGrid(container, noOfGrid){
+    removeAllBoxes();
+
+    // let noOfGrid = prompt("No of grid?");
+    const widthOfContainer = container.clientWidth;
+    const widthOfGrid = widthOfContainer / noOfGrid;
+    let isMouseDown = false;
+    for (let i = 1; i <= noOfGrid * noOfGrid; ++i) {
+        const grid = document.createElement("div");
+        grid.setAttribute("style", `width: ${widthOfGrid}px; border: 1px solid black; box-sizing: border-box;`);
+
+        //Trying to add all event listeners in the same loop
+        grid.addEventListener("mousedown", (e) => {
+            isMouseDown = true;
+            e.target.style.background = `${color}`;
+        });
+        grid.addEventListener("mouseup", () => isMouseDown = false);
+
+        grid.addEventListener("mouseover", (e) => {
+            if (isMouseDown === true) {
+                e.target.style.background = `${color}`;
+            }
+        });
+
+        container.appendChild(grid);
+    }
+
+}
